596a570f61427058a284225c471e1d5ede97c679
Stuart Jauncey <stuart.jauncey@shift.online>
2023-10-24 16:58:31 +0100

    Create card menu config

---
+++ app/configs/menus/cards/index.ts
@@ -0,0 +1,14 @@
+const cardMenuConfig = {
+  options: [
+    {
+      name: "View Cards",
+      link: "/cards/my-cards",
+    },
+    {
+      name: "Add Cards",
+      link: "/cards/add-card",
+    },
+  ],
+};
+
+export default cardMenuConfig;
