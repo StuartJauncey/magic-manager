dfb223a40fc340d7ffc6b9efcf135c75db0b5c2f
Stuart Jauncey <stuart.jauncey@shift.online>
2023-10-24 16:58:22 +0100

    Create main menu config

---
+++ app/configs/menus/main/index.ts
@@ -0,0 +1,18 @@
+const mainMenuConfig = {
+  options: [
+    {
+      name: "Cards",
+      link: "/cards",
+    },
+    {
+      name: "Decks",
+      link: "/decks",
+    },
+    {
+      name: "Tournaments",
+      link: "/tournaments",
+    },
+  ],
+};
+
+export default mainMenuConfig;
